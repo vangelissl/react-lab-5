b9090b691e0375313b1f29cce0d7c8d8b364ede3
Maria Stashenko <mahryia@gmail.com>
2025-11-29 23:10:30 +0200

    add footer

---
+++ hi-tech-website/src/Footer.jsx
@@ -0,0 +1,24 @@
+
+
+function Footer() {
+	return (
+		<footer>
+			<h2>Contact SecureYourCity</h2>
+			<address>
+				Address: 123 Innovation Avenue, Kyiv, Ukraine<br />
+				Phone: +380-67-183-7825<br />
+				Email: <a href="mailto: info@syc.com">info@syc.com</a>
+			</address>
+			<br />
+			<nav>
+				<a href="index.html">Home</a> |
+				<a href="projects.html">Projects</a> |
+				<a href="contact.html">Contact</a>
+			</nav>
+			<br />
+			<p>© 2025 SecureYourCity (SYC). All rights reserved.</p>
+		</footer>
+	)
+}
+
+export default Footer
