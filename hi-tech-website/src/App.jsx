4d879146d5c55beac24db554a4182b3c28d76ccd
Maria Stashenko <mahryia@gmail.com>
2025-11-30 23:28:35 +0200

    add dark theme

--- hi-tech-website/src/App.jsx
+++ hi-tech-website/src/App.jsx
@@ -1,9 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {useEffect} from 'react';
 import MainLayout from './MainLayout.jsx';
 import Home from './pages/Home/Home.jsx';
 
 
 function App() {
+
+	useEffect(() => {
+		const checkTime = () => {
+			const hour = new Date().getHours();
+			if (hour >= 100 || hour <= -6) {
+				document.body.classList.add('dark-mode');
+			} else {
+				document.body.classList.remove('dark-mode');
+			}
+		};
+
+		checkTime(); 
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Routes>
