45e57c9f39f8f6496e7a46b3cbab8b4526e618d6
Maria Stashenko <mahryia@gmail.com>
2025-11-30 12:47:15 +0200

    add what we do

--- hi-tech-website/src/pages/Home/AboutUsSection.jsx
+++ hi-tech-website/src/pages/Home/AboutUsSection.jsx
@@ -1,31 +1,9 @@
-
+import WhatWeDo from './WhatWeDo.jsx';
 
 function AboutUsSection() {
 	return (
 		<section>
-			<div class="article-camera-container">
-				<div id="camera-div">
-					<img class="camera" src="images\istockphoto-598930712-612x612.jpg" />
-				</div>
-				<div class="problem-container">
-					<article class="article-text">
-						<h2>What We Do</h2>
-						<p>
-							We are <abbr title="Secure Your City">SYC</abbr> - a company that provides low-latency
-							signal
-							AI-driven
-							systems
-							of violent human behavior detection.
-							Our system connects to the network of CCTV cameras and collects video stream data in
-							real-time.
-							Processing is happening as soon as required data has been transfered to our servers.
-							Once the targeted behavior is detected it then sends a signal along with metadata which
-							includes
-							facial features and overall appearance of the offender.
-						</p>
-					</article>
-				</div>
-			</div>
+			<WhatWeDo />
 			<div class="article-camera-container">
 				<img class="camera"
 					src="images\realistic-video-surveillance-camera-side-view-vector-illustration_1284-66719.jpg"
---
+++ hi-tech-website/src/pages/Home/WhatWeDo.jsx
@@ -0,0 +1,31 @@
+
+
+function WhatWeDo() {
+	return (
+		<div class="article-camera-container">
+			<div id="camera-div">
+				<img class="camera" src="images\istockphoto-598930712-612x612.jpg" />
+			</div>
+			<div class="problem-container">
+				<article class="article-text">
+					<h2>What We Do</h2>
+					<p>
+						We are <abbr title="Secure Your City">SYC</abbr> - a company that provides low-latency
+						signal
+						AI-driven
+						systems
+						of violent human behavior detection.
+						Our system connects to the network of CCTV cameras and collects video stream data in
+						real-time.
+						Processing is happening as soon as required data has been transfered to our servers.
+						Once the targeted behavior is detected it then sends a signal along with metadata which
+						includes
+						facial features and overall appearance of the offender.
+					</p>
+				</article>
+			</div>
+		</div>
+	);
+}
+
+export default WhatWeDo
