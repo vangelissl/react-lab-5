2cb2dd92cc5aabf5e635f428ed0626540a67b366
Maria Stashenko <mahryia@gmail.com>
2025-11-30 20:15:14 +0200

    add app data directory

---
+++ hi-tech-website/src/data/data.js
@@ -0,0 +1,32 @@
+export const foundersList = [
+	{
+		name: 'Maria Stashenko',
+		role: 'Founder & Head of Product Innovation',
+		background: '3+ years in software enginering, student at KNU',
+		isFounder: true
+	},
+	{
+		name: 'Alex Carter',
+		role: 'CEO',
+		background: '10+ years in cybersecurity, ex-Google',
+		isFounder: false
+	},
+	{
+		name: 'Sophia Lee',
+		role: 'CTO',
+		background: 'PhD in AI, led research at MIT',
+		isFounder: false
+	},
+	{
+		name: 'David Krasinski',
+		role: 'COO',
+		background: 'Operations strategist, ex-Microsoft',
+		isFounder: false
+	},
+	{
+		name: 'Maria Rossi',
+		role: 'CFO',
+		background: 'Finance expert, worked at Goldman Sachs',
+		isFounder: false
+	}
+]
