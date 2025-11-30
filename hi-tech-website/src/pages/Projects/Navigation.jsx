bb7305844c43be8ec0cc1fefe46e08544296c139
Maria Stashenko <mahryia@gmail.com>
2025-12-01 00:01:03 +0200

    add projects navigation

---
+++ hi-tech-website/src/pages/Projects/Navigation.jsx
@@ -0,0 +1,22 @@
+
+
+function Navigation() {
+	return (
+		<nav>
+			<ol id="side-nav">
+				<li><a href="#kyiv-project">Safe Kyiv Initiative</a></li>
+				<li><a href="#warsaw-project">Secure Warsaw Program</a></li>
+				<li><a href="#berlin-project">Berlin Safety Net</a></li>
+				<li><a href="#london-project">London Guardian Network</a></li>
+				<li><a href="#toronto-project">Safe Toronto Vision</a></li>
+				<li><a href="#tokyo-project">Tokyo City Shield</a></li>
+				<li><a href="#paris-project">Paris Urban Safety Grid</a></li>
+				<li><a href="#new-york-city-project">New York City Safety Program</a></li>
+				<li><a href="#madrid-project">Madrid Smart Safety</a></li>
+				<li><a href="#stockholm-project">Stockholm Safe Future</a></li>
+			</ol>
+		</nav>
+	);
+}
+
+export default Navigation
--- hi-tech-website/src/pages/Projects/Projects.jsx
+++ hi-tech-website/src/pages/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-
+import Navigation from './Navigation.jsx';
 
 function Projects() {
 	return (
@@ -9,20 +9,7 @@ function Projects() {
 			<div class="container">
 				<aside id="project-list">
 					<h1>Our Projects</h1>
-					<nav>
-						<ol id="side-nav">
-							<li><a href="#kyiv-project">Safe Kyiv Initiative</a></li>
-							<li><a href="#warsaw-project">Secure Warsaw Program</a></li>
-							<li><a href="#berlin-project">Berlin Safety Net</a></li>
-							<li><a href="#london-project">London Guardian Network</a></li>
-							<li><a href="#toronto-project">Safe Toronto Vision</a></li>
-							<li><a href="#tokyo-project">Tokyo City Shield</a></li>
-							<li><a href="#paris-project">Paris Urban Safety Grid</a></li>
-							<li><a href="#new-york-city-project">New York City Safety Program</a></li>
-							<li><a href="#madrid-project">Madrid Smart Safety</a></li>
-							<li><a href="#stockholm-project">Stockholm Safe Future</a></li>
-						</ol>
-					</nav>
+					<Navigation />
 				</aside>
 				<main className="victor-mono-regular">
 					<ol className="project-list-container">
