2fec0cb9f35c7640b7c75ac10c51f37a510dbea1
Maria Stashenko <mahryia@gmail.com>
2025-11-30 12:48:58 +0200

    add what we solve

---
+++ hi-tech-website/src/pages/Home/WhatWeSolve.jsx
@@ -0,0 +1,24 @@
+
+
+function WhatWeSolve() {
+	return (
+		<div class="article-camera-container">
+			<img class="camera"
+				src="images\realistic-video-surveillance-camera-side-view-vector-illustration_1284-66719.jpg"
+				height="250em" />
+			<div class="problem-container">
+				<article class="article-text">
+					<h2>What Problem Do We Solve</h2>
+					<p id="purpose">
+						Our system operates on a network of CCTV cameras to help you detect
+						and identify criminals faster. We help reduce crimes by identfying them during occurence
+						Today, almost every street of any city is full of survalliance cameras.
+						While they provide assistance in solving a mystery - we help do it in time.
+					</p>
+				</article>
+			</div>
+		</div>
+	);
+}
+
+export default WhatWeSolve
