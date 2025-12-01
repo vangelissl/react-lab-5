ec2035c2d6466ab982bca7fb1641b7230138a5cf
Maria Stashenko <mahryia@gmail.com>
2025-12-01 11:49:16 +0200

    add testimonials provider

--- hi-tech-website/src/pages/Testimonials/Testimonials.jsx
+++ hi-tech-website/src/pages/Testimonials/Testimonials.jsx
@@ -1,10 +1,13 @@
-
+import Grid from './Grid.jsx';
+import { TestimonialsProvider } from '../../contexts/TestimonialsContext.jsx';
 
 function Testimonials() {
 	return (
 		<main>
 			<h1>Testimonials From Our Partners</h1>
-			
+			<TestimonialsProvider>
+				<Grid />
+			</TestimonialsProvider>
 		</main>
 	);
 }
